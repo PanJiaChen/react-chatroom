dd388f0ebd3f02e1ada5d4f723bdba5ce8253568
Pan <panfree23@gmail.com>
2015-12-16 16:33:22 +0800

    音视频提供回放功能

--- assets/src/pc_r/js/containers/video/VideoContainer.js
+++ assets/src/pc_r/js/containers/video/VideoContainer.js
@@ -20,26 +20,35 @@ class VideoContainer extends Component {
         const state = store.getState();
         const hasInit=$('#audio-mount').hasClass('hasInit')
         if(state.results.length>0 && !hasInit){
-            var url=state.results[0].url['liveHls'];
-                var srcPath =(
-                    "http://wscn.cdn.wallstreetcn.com/wscn/js/swise-player/sewise.player.min.js?"+
-                    "server=vod&"+
-                    "type=m3u8&"+
-                    "autostart=true"+
-                    "&starttime=0&"+
-                    "buffer=2&"+
-                    "title=华尔街见闻&"+
-                    "lang=en_US&"+
-                    "claritybutton=disable&"+
-                    "poster=http://wscn.cdn.wallstreetcn.com/wscn/img/logo@2x.png&"+
-                    "skin=vodTransparent&"+
-                    "videourl="+url
-                )
-                var script = document.createElement('script');
-                script.type = "text/javascript";
-                script.src = srcPath ;
-                //用JQ的append方法动态添加脚本会造成脚本被执行两次，所以这里改为原生动态添加脚本的方式。
-                $("#video-mount").addClass('hasInit').get(0).appendChild(script);
+            const end=state.results[0].disp['end'];
+            const now=Date.parse(new Date())/1000;
+            if(now>end){
+                //重播
+                var url=state.results[0].url['playbackHls'].split('?')[0];
+                url=url+'%3Fstart=0%26end=0'
+            }else{
+                //直播
+                var url=state.results[0].url['liveHls'];
+            }
+            var srcPath =(
+                "http://wscn.cdn.wallstreetcn.com/wscn/js/swise-player/sewise.player.min.js?"+
+                "server==vod&"+
+                "type==m3u8&"+
+                "autostart==true"+
+                "&starttime==0&"+
+                "buffer==2&"+
+                "title==华尔街见闻&"+
+                "lang==en_US&"+
+                "claritybutton==disable&"+
+                "poster==http://wscn.cdn.wallstreetcn.com/wscn/img/logo@2x.png&"+
+                "skin==vodTransparent&"+
+                "videourl=="+url
+            )
+            var script = document.createElement('script');
+            script.type = "text/javascript";
+            script.src = srcPath ;
+            //用JQ的append方法动态添加脚本会造成脚本被执行两次，所以这里改为原生动态添加脚本的方式。
+            $("#video-mount").addClass('hasInit').get(0).appendChild(script);
         }
         return (
             <div id='video-mount' className='video-container'>
